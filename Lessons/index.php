1b5034a8dc7bdf676185ec90638f6b116b5a3f75
Denis Gadalshin <g-d-g@bk.ru>
2021-12-19 17:42:59 +0300

    Перенес учебные файлы в папку Lessons

---
+++ Lessons/index.php
@@ -0,0 +1,12 @@
+<?PHP
+
+//require_once 'funcs.php';
+//require_once 'funcs.php';
+
+include 'funcs.php';
+
+include_once 'funcs.php';
+
+
+
+test();
--- index.php
+++ index.php
@@ -1,12 +1,2 @@
-<?PHP
+<?php
 
-//require_once 'funcs.php';
-//require_once 'funcs.php';
-
-include 'funcs.php';
-
-include_once 'funcs.php';
-
-
-
-test();
